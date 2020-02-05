4d9cda85cbcf1d35069bb35a6978fe9d5b3fc216
root <1530204@upv.edu.mx>
2020-02-05 10:52:45 +0000

    backup

---
+++ wp-content/themes/blossom-feminine/functions.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Blossom Feminine functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Blossom_Feminine
+ */
+
+//define theme version
+if ( ! defined( 'BLOSSOM_FEMININE_THEME_VERSION' ) && ! defined( 'BLOSSOM_FEMININE_THEME_NAME' ) && ! defined( 'BLOSSOM_FEMININE_THEME_TEXTDOMAIN' ) ) {
+	$theme_data = wp_get_theme();	
+	define ( 'BLOSSOM_FEMININE_THEME_VERSION', $theme_data->get( 'Version' ) );
+	define ( 'BLOSSOM_FEMININE_THEME_NAME', $theme_data->get( 'Name' ) );
+	define ( 'BLOSSOM_FEMININE_THEME_TEXTDOMAIN', $theme_data->get( 'TextDomain' ) );
+}
+
+/**
+ * Implement the Custom functions.
+ */
+require get_template_directory() . '/inc/custom-functions.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Custom functions for selective refresh.
+ */
+require get_template_directory() . '/inc/partials.php';
+
+/**
+ * Metabox
+ */
+require get_template_directory() . '/inc/metabox.php';
+
+/**
+ * Custom Controls
+ */
+require get_template_directory() . '/inc/custom-controls/custom-control.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Widget Areas.
+ */
+require get_template_directory() . '/inc/widgets.php';
+
+/**
+ * Dynamic Styles
+ */
+require get_template_directory() . '/css/style.php';
+
+/**
+ * Typography Functions
+ */
+require get_template_directory() . '/inc/typography.php';
+
+/**
+ * Plugin Recommendation
+*/
+require get_template_directory() . '/inc/tgmpa/recommended-plugins.php';
+
+/**
+ * Add theme compatibility function for woocommerce if active
+*/
+if( blossom_feminine_is_woocommerce_activated() ){
+    require get_template_directory() . '/inc/woocommerce-functions.php';    
+}
+
+/**
+ * Toolkit Filters
+*/
+if( blossom_feminine_is_bttk_activated() )
+require get_template_directory() . '/inc/toolkit-functions.php';
+
+/**
+ * Getting Started
+*/
+require get_template_directory() . '/inc/getting-started/getting-started.php';
